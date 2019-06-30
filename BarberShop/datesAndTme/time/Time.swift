e1548fb933e8c5fe1f321499f32830823731ffb1
radshun <daniel.rad.b7@gmail.com>
2019-06-30 09:41:55 +0300

    time manager

---
+++ BarberShop/datesAndTme/time/Time.swift
@@ -0,0 +1,26 @@
+//
+//  Time.swift
+//  BarberShop
+//
+//  Created by Daniel Radshun on 28/06/2019.
+//  Copyright © 2019 Neighbors. All rights reserved.
+//
+
+import Foundation
+
+struct Time:CustomStringConvertible {
+    var hours:Int
+    var minutes:Int
+    
+    var description: String{
+        if minutes < 10{
+            return "\(hours):0\(minutes)"
+        }
+        return "\(hours):\(minutes)"
+    }
+}
+
+struct TimeRange {
+    var fromTime:Time
+    var toTime:Time
+}
