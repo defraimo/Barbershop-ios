62b51c9a7d6889c464b26cad4c321a3a3edc8566
radshun <daniel.rad.b7@gmail.com>
2019-07-04 15:05:15 +0300

    choose services

--- BarberShop/makingAppointment/chooseHaircut/cell/HaircutTableViewCell.swift
+++ BarberShop/makingAppointment/chooseHaircut/cell/HaircutTableViewCell.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class HaircutTableViewCell: UITableViewCell {
-    @IBOutlet weak var haircutType: UILabel!
+    @IBOutlet weak var haircutType: UIButton!
     @IBOutlet weak var infoButton: UIButton!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var barbersCollection: UICollectionView!
+    
+    var barbers:[Barber]?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        barbersCollection.delegate = self
+        barbersCollection.dataSource = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -22,5 +29,25 @@ class HaircutTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func setBarbers(_ barbers:[Barber]){
+        self.barbers = barbers
+    }
 
+}
+
+extension HaircutTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return barbers?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "barbersHaircut", for: indexPath) as! HaircutDetailsCollectionViewCell
+        
+        cell.barberImage.image = barbers?[indexPath.row].image
+        
+        return cell
+    }
+    
+    
 }
--- BarberShop/makingAppointment/chooseHaircut/controller/ChooseHaircutViewController.swift
+++ BarberShop/makingAppointment/chooseHaircut/controller/ChooseHaircutViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class ChooseHaircutViewController: UIViewController {
+    @IBOutlet weak var chooseServiesTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //setting the background
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
+        
+        infoOrClose = #imageLiteral(resourceName: "info_icon")
     }
 
 }
@@ -21,15 +25,64 @@ class ChooseHaircutViewController: UIViewController {
 let prices = PricesDataSource().pricesList
 //to do singleton
 
+var isDetailsShown = false
+var chosenCellRow:Int?
+var infoOrClose:UIImage?
+
 extension ChooseHaircutViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return prices.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "haircutCell") as! HaircutTableViewCell
+        
+        let haircut = prices[indexPath.row]
+        
+        cell.haircutType.setTitle(haircut.servies, for: .normal)
+        cell.priceLabel.text = haircut.price
+        cell.setBarbers(barbers)
+        
+        cell.infoButton.setImage(infoOrClose, for: .normal)
+        
+        cell.haircutType.tag = indexPath.row
+        cell.haircutType.addTarget(self, action: #selector(haitcutChosen(_:)), for: .touchUpInside)
+        
+        cell.infoButton.tag = indexPath.row
+        cell.infoButton.addTarget(self, action: #selector(showDetails(_:)), for: .touchUpInside)
+        
+        cell.sizeToFit()
+        
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        if indexPath.row == chosenCellRow && isDetailsShown{
+            return 180
+        }
+        
+        return 80
+    }
+    
+    @objc func haitcutChosen(_ sender:UIButton){
+        let chosenIndex = sender.tag
+        performSegue(withIdentifier: "toChooseBarber", sender: nil)
+    }
+    
+    @objc func showDetails(_ sender:UIButton){
+        isDetailsShown = !isDetailsShown
+        if isDetailsShown{
+            infoOrClose = #imageLiteral(resourceName: "close_icon")
+        }
+        else{
+            infoOrClose = #imageLiteral(resourceName: "info_icon")
+        }
+        
+        chosenCellRow = sender.tag
+        
+        chooseServiesTable.reloadRows(at: [IndexPath(row: chosenCellRow!, section: 0)], with: .automatic)
+    }
     
 }
