be4d514fcaf272717d487adc607f126c9d63fc07
fraimo <framiframo96@gmail.com>
2019-07-27 18:57:31 +0300

    started custom alert

---
+++ BarberShop/customAlert/AlertService.swift
@@ -0,0 +1,13 @@
+
+//  AlertService.swift
+//  BarberShop
+//
+//  Created by Daniel Fraimovich on 25/07/2019.
+//  Copyright © 2019 Neighbors. All rights reserved.
+//
+
+import UIKit
+
+class AlertService{
+    
+}
---
+++ BarberShop/customAlert/AlertViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AlertViewController.swift
+//  BarberShop
+//
+//  Created by Daniel Fraimovich on 25/07/2019.
+//  Copyright © 2019 Neighbors. All rights reserved.
+//
+
+import UIKit
+
+class AlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
